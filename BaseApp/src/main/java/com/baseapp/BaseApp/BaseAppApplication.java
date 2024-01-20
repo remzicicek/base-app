c8d992f0319fa68877e18d9f905a95c23b38b83c
remzicicek <remzicicek61@gmail.com>
2024-01-20 17:22:51 +0300

    swagger is added

--- BaseApp/src/main/java/com/baseapp/BaseApp/BaseAppApplication.java
+++ BaseApp/src/main/java/com/baseapp/BaseApp/BaseAppApplication.java
@@ -1,9 +1,29 @@
 package com.baseapp.BaseApp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Base App Project",
+				version = "1.0.0",
+				description = "This project is only for researching",
+				termsOfService = "base app for researching",
+				contact = @Contact(
+							name = "Base App",
+							email = "baseapp@baseappmail.com"
+				),
+				license = @License(
+						name = "licence",
+						url = "baseapp"
+				)
+		)
+)
 public class BaseAppApplication {
 
 	public static void main(String[] args) {
